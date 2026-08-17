de5b51f28ec5bfff857bdd8ea7addc7b610b4d57
mycomind4-arch <mycomind4@gmail.com>
2026-08-16 18:55:47 -0700

    feat(platform): expose Small Business integration contracts

---
+++ src/platform/index.ts
@@ -0,0 +1,4 @@
+export { smallBusinessManifest } from './vertical-manifest'
+export type { VerticalManifest, RiskLevel } from './vertical-manifest'
+export { evaluateWorkflowPolicy } from './workflow-policy'
+export type { WorkflowPolicyInput, WorkflowPolicyResult } from './workflow-policy'
